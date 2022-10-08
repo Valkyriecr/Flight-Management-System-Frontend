4cb57382928fc6e01feb0f424f2f38d24c7da05c
Valkyriecr <erowan95@gmail.com>
2022-10-08 21:08:34 +0200

    Checkin updated post team discuss/LoginView

--- src/views/CheckInView.vue
+++ src/views/CheckInView.vue
@@ -18,26 +18,32 @@
           label="ID Number"
           hide-details="auto"
           :rules="IdRule"
+          value="9510095080080"
+          disabled
         ></v-text-field>
-        <v-spacer class="pa-5"></v-spacer>
-        <v-combobox
-          v-model="selectTicket"
-          :tickets="Ticket"
-          label="Tickets"
-          hide-selected
-          outlined
-          dense
-        ></v-combobox>
+        <v-text-field
+          label="Ticket ID"
+          hide-details="auto"
+          :rules="rules"
+          value="TK9510"
+          disabled
+        ></v-text-field>
+
         <v-text-field label="Flight ID" hide-details="auto"></v-text-field>
       </div>
       <v-spacer class="pa-5"></v-spacer>
       <div>
         <h3>Luggage</h3>
-        <v-text-field
-          label="Luggage Type"
-          hide-details="auto"
-          :rules="rules"
-        ></v-text-field>
+        <v-spacer class="pa-2"></v-spacer>
+        <v-combobox
+          v-model="selectLuggage"
+          :items="luggageType"
+          label="Luggage"
+          hide-selected
+          clearable
+          outlined
+          dense
+        ></v-combobox>
         <v-text-field
           class="pb-6"
           label="Luggage Weight"
@@ -70,22 +76,21 @@ export default {
       selectTicket: ["Select Ticket"],
       selectArrival: ["Destination Location"],
       items: ["FA-262", "FA-202", "FA-200", "FA-203"],
-      ticket:["Tk1234"],
+      luggageType: ["Carry-on", "Checked-In","Sports","Weapons","Animals"],
 
-      checkin:{
-        ticketId:null,
-        FirstName:"",
-        LastName:"",
-        IdNumber:"",
-        FlightId:"",
+      checkin: {
+        ticketId: null,
+        FirstName: "",
+        LastName: "",
+        IdNumber: "",
+        FlightId: "",
       },
-      luggage:{
-        weight:null,
-        luggageType:"",
-        passenger:"????",
-      }
+      luggage: {
+        weight: null,
+        luggageType: "",
+        passenger: "????",
+      },
     };
   },
-  
 };
 </script>
